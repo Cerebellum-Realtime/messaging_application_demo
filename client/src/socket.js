9f9a9b47d4d754d774942934ec43b8735123e9ff
Dylan Spyer <dylanspyer@gmail.com>
2024-07-03 13:33:52 -0500

    Co-authored-by: Will <wconrad265@users.noreply.github.com>

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,14 @@ import { io } from "socket.io-client";
 
 const URL = "ALB-ws-1063097076.us-east-1.elb.amazonaws.com";
 
+//Avery: ALB-WS-Server-958728794.us-east-1.elb.amazonaws.com
+//will's url: ALB-for-backend-health-1634376545.us-east-2.elb.amazonaws.com
+
 export const socket = io(URL, {
-  path: "/socket.io",
-  transports: ["websocket"], // Include both websocket and polling as fallback
+  withCredentials: true,
+
+  // path: "/socket.io",
+  transports: ["polling"], // Include both websocket and polling as fallback
 });
+
+// Ensure credentialed requests are not sent to CORS resources with origin wildcards
