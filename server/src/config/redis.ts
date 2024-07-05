d6d12abd8857299b1594cf28baada523cce1a9e4
Will Conrad <w.conrad@outlook.com>
2024-07-04 20:29:22 -0500

    Build: Rewrote Docker file and redis.ts to handle TypeScript

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,23 +1,20 @@
 import { Redis } from "ioredis";
 
-//Have to do for TypeScript
-const redisPort: number = process.env.REDIS_ENDPOINT_PORT
-  ? parseInt(process.env.REDIS_ENDPOINT_PORT)
-  : 6379;
+const redisHost = process.env.REDIS_ENDPOINT_ADDRESS || "redis";
+const redisPort = parseInt(process.env.REDIS_ENDPOINT_PORT || "6379", 10);
 
-export const sub = new Redis({
-  host: process.env.REDIS_ENDPOINT_ADDRESS,
+const redisConfig = {
+  host: redisHost,
   port: redisPort,
-})
+};
+
+export const sub = new Redis(redisConfig)
   .on("ready", () => console.log("Subscriber Redis client is ready"))
   .on("error", (error) => {
     console.error("Subscriber Redis client error:", error);
   });
 
-export const pub = new Redis({
-  host: process.env.REDIS_ENDPOINT_ADDRESS,
-  port: redisPort,
-})
+export const pub = new Redis(redisConfig)
   .on("ready", () => console.log("Publisher client is ready"))
   .on("error", (error) => {
     console.error("Publisher Redis client error:", error);
